bb89304382d722b0eb9b4a4be09dd12c04785ace
Prince Bassey <basseyprince75@gmail.com>
2022-02-16 19:26:17 +0100

    updated local database

--- src/api/router.js
+++ src/api/router.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+const userRoute = require('../controllers/userController')
 
 const fileService = require('./convert');
 
 router.post('/upload', upload.single('file'), fileService.convert);
+router.post('/api/v1/create' , userRoute.createUser)
+router.post('/api/v1/login' , userRoute.login)
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,35 +1,42 @@
+//require the dotenv
+require("dotenv").config();
 // Import modules
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const logger = require('morgan')
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const logger = require("morgan");
 
 const app = express();
 
 // setup cors / helmet
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(helmet());
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // setup body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Router
-app.get('/', (req, res) => res.json({ message: 'API to the cloud... 🚀' }));
-app.use(require('./api/router'));
+// connect the database
+require("./database/db").connect();
 
+// Router
+app.get("/", (req, res) => res.json({ message: "API to the cloud... 🚀" }));
+app.use(require("./api/router"));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`);
 });
 
 module.exports = app;
